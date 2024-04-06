2a0003345fd860c62b03be66283f283a6f768232
Aleksei Golubkov <nuriver@gmail.com>
2024-04-06 17:26:37 +0200

    feat: solve getAllResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -134,8 +134,21 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return new Promise((resolve) => {
+    const dataArr = [];
+    const dataForPromise = Promise.allSettled(promises).then((results) => {
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          dataArr.push(result.value);
+        } else {
+          dataArr.push(null);
+        }
+      });
+      return dataArr;
+    });
+    dataForPromise.then((data) => resolve(data));
+  });
 }
 
 /**
